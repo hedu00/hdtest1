b18124eb97b04b73ff8bb060a79ddc704801cd7c
何杜 <hedu@corp.netease.com>
2021-01-23 16:27:58 +0800

    Initial commit

---
+++ src/main/java/com/example/hdtest1/Hdtest1Application.java
@@ -0,0 +1,25 @@
+package com.example.hdtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class Hdtest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hdtest1Application.class, args);
+	}
+	@GetMapping("/getdate")
+	public String getdate(){
+
+		Date date = new Date() ;
+
+		return String.format(" --> NSF: hdtest1 v2: " + date.toString() + " <-- \n");
+
+	}
+}
---
+++ src/test/java/com/example/hdtest1/Hdtest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdtest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hdtest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
